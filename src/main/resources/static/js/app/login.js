c3a6e9f7028e4f77ad124a79ef6c455c5484f794
Mayank Kataruka <mayank.kt9@gmail.com>
2019-10-19 10:15:17 -0700

    Login Ajax Call Functionality implemented

--- src/main/resources/static/js/app/login.js
+++ src/main/resources/static/js/app/login.js
@@ -9,7 +9,35 @@
        loginJson["dob"] = dob;
        jsonWrapper["login"] = loginJson;
        console.log(jsonWrapper);
+       loginAjaxCall(jsonWrapper);
      
      
      
+     }
+     
+     function loginAjaxCall(jsonWrapper){
+            
+            
+            $.ajax({url: "https://api.myjson.com/bins/iwdug", 
+                   
+                   success: function(data){
+                   data = data["login"];
+                   if(data["result"] == "success"){
+                          if( data["grade"] == "1" ){
+                               window.location.href = "index.html";
+                          }
+                          else if(data["grade"] == "2"){
+                               window.location.href = "index1.html";
+                          } 
+                   }
+                   },
+                   error:function (e){
+                        console.log("Error during Login JSON Call");
+                   }
+            
+            
+            });
+            
+            
+            
      }
