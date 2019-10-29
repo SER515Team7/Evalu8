8ab841e39fd5535b362f7e6b1c1ba7bb7608208f
nlakshm <nlakshm3@asu.edu>
2019-10-29 16:07:55 -0700

    navigation to different pages according to grades

--- src/main/resources/static/js/app/login.js
+++ src/main/resources/static/js/app/login.js
@@ -3,13 +3,31 @@
        userName = document.getElementById("userName").value;
        dob  = document.getElementById("dob").value;
      
-       jsonWrapper ={};
+     
        var loginJson = {};
        loginJson["username"] = userName;
        loginJson["dob"] = dob;
-       jsonWrapper["login"] = loginJson;
-       console.log(jsonWrapper);
+        ajaxCallForLogin(loginJson);
      
      
      
+     }
+
+
+     function ajaxCallForLogin(loginJson){
+       grade = 3;
+       navigate(grade);
+     }
+
+     function navigate(grade){
+          localStorage.setItem("grade",grade);
+          if(grade <=3){
+            window.location.href = ("index.html");
+          }
+          else if( grade >3 && grade <=8){
+            window.location.href = ("index1.html");
+          }
+          else if(grade >8 && grade <=12){
+            window.location.href = ("index2.html");
+          }
      }
