77b63030ce2c22cf4abe1c7adedefe406eaa3425
Ashutosh Dey <adey13@asu.edu>
2019-10-31 17:26:31 -0700

    controller and service integrated with javascript frontend

--- src/main/java/com/asu/project7/controller/SandboxController.java
+++ src/main/java/com/asu/project7/controller/SandboxController.java
@@ -1,19 +1,11 @@
 package com.asu.project7.controller;
 
-import com.asu.project7.dto.StudentDTO;
 import com.asu.project7.model.Student;
 import com.asu.project7.repository.StudentRepository;
 import com.asu.project7.service.SandboxService;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.boot.json.GsonJsonParser;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RequestMapping("/sandbox")
 @RestController
@@ -25,16 +17,18 @@ public class SandboxController {
     SandboxService sandboxService;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public ResponseEntity<Student> verifyLogin(@RequestParam int studentId )  {
+    public ResponseEntity<Student> verifyLogin(@RequestParam int studentId) {
 
-        System.out.println("Received studentId:"+studentId);
-        Student student=new Student();
+        System.out.println("Received studentId:" + studentId);
+        Student student = new Student();
         student.setStudentId(studentId);
+//        ResponseEntity<Student> x = this.sandboxService.verifyLogin(student);
+//        System.out.println(x.toString());
         return this.sandboxService.verifyLogin(student);
     }
 
-    @RequestMapping(value = "/addStudent",method = RequestMethod.POST)
-    public void addStudent(@RequestBody Student student){
+    @RequestMapping(value = "/addStudent", method = RequestMethod.POST)
+    public void addStudent(@RequestBody Student student) {
         this.sandboxService.addStudent(student);
     }
 
