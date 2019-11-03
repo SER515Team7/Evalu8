a41a610e0d0e15093831137a350c765e832e8797
Ashutosh Dey <adey13@asu.edu>
2019-11-02 18:01:02 -0700

    resolving conflicts for index.html, login.js and login.html

--- src/main/resources/static/js/app/login.js
+++ src/main/resources/static/js/app/login.js
@@ -1,4 +1,4 @@
- document.getElementById("errorMessage").style.display="none";  
+ document.getElementById("errorMessage").style.display="none";
      function validateLogin(){
        userName = document.getElementById("userName").value;
        dob  = document.getElementById("dob").value;
@@ -13,30 +13,60 @@
 
      }
 
-     function ajaxCallForLogin(loginJson){
-       grade = 9;
+
+document.getElementById("errorMessage").style.display = "none";
+
+function validateLogin() {
+    userName = document.getElementById("userName").value;
+    dob = document.getElementById("dob").value;
 
 
-      $.ajax({url: "http://localhost:8080/sandbox/login", 
-       
-       success: function(result){
-           console.log(result);
-       }
-    
-      });
-       navigate(grade);
-     }
+    var loginJson = {};
+    loginJson["studentId"] = userName;
+    loginJson["dateOfBirth"] = dob;
+    ajaxCallForLogin(loginJson);
+    console.log(loginJson);
 
-     function navigate(grade){
-          localStorage.setItem("grade",grade);
-          if(grade <=3){
-            window.location.href = ("index.html");
-          }
-          else if( grade >3 && grade <=8){
-            window.location.href = ("index1.html");
-          }
-          else if(grade >8 && grade <=12){
-            window.location.href = ("index2.html");
-          }
 
-     }
+}
+
+
+function ajaxCallForLogin(loginJson) {
+    grade = 3;
+    document.getElementById("errorMessage").style.display ="none";
+    myJson = JSON.stringify(loginJson);
+    urlLink = "http://localhost:8080/sandbox/login/" + myJson;
+    $.ajax({
+        url: "http://localhost:8080/sandbox/login/",
+        data: loginJson,
+        success: function (result) {
+            console.log(result);
+
+            if (result['status'] != 404) {
+                navigate(result['grade']);
+            } else {
+                document.getElementById("errorMessage").style.display ="block";
+                document.getElementById("errorMessage").innerHTML = "Invalid Login Credentials";
+            }
+        },
+        error: function (result) {
+            document.getElementById("errorMessage").innerHTML = "Invalid Login  Credentials";
+            console.log("error message");
+            document.getElementById("errorMessage").style.display ="block";
+        }
+
+    });
+
+}
+
+function navigate(grade) {
+    localStorage.setItem("grade", grade);
+    if (grade <= 3) {
+        window.location.href = ("index.html");
+    } else if (grade > 3 && grade <= 8) {
+        window.location.href = ("index1.html");
+    } else if (grade > 8 && grade <= 12) {
+        window.location.href = ("index2.html");
+    }
+}
+
