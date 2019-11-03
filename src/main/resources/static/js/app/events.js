eda3225243ce82ac95030dd02fc9d879defb3fe9
Nabeel <nabeel@nabeels-mbp.3500.dhcp.asu.edu>
2019-11-02 17:22:20 -0700

    Expression evaluation string construction from ui and db call evaluating the constructed expression

--- src/main/resources/static/js/app/events.js
+++ src/main/resources/static/js/app/events.js
@@ -17,11 +17,53 @@ function getExpression(){
         if(expression[i].nodeName != "#text"){
         var child = expression[i];
         var text = child.getElementsByClassName("numberOperatorText")[0];
-        
+             if(text != undefined){
+                if(text.innerHTML.replace(" ","") == "x"){
+                    builtExp += "*";
+                    console.log("+ here");
+
+                }
+                else{
+                    builtExp += (text.innerHTML);
+                    console.log(text.innerHTML);
+
+                }
+            }
         }
          
     }
     dbCallForExpressionValidation(builtExp);
+}
+
+
+function dbCallForExpressionValidation(exp){
+
+    expJson ={}
+    expJson["expression"] = exp.replace(" ","");
+
+      console.log(expJson);
+
+
+
+    $.ajax({
+        url: "http://localhost:8080/sandbox/validate/",
+        data: expJson,
+        success: function (result) {
+            console.log(result);
+
+            if (result['status'] != 404) {
+                 document.getElementById("result").innerHTML = result["result"][0];
+            } else {
+
+            }
+        },
+        error: function (result) {
+
+        }
+
+    });
+
+
 }
 
 
