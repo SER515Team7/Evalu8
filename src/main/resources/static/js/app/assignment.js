a2b58ae74f7408f073ed7c9de4e411d971a43514
nlakshm <nlakshm3@asu.edu>
2019-11-22 16:47:25 -0700

    ÜS_54_Assignment database call to store the  newly created  assignment

--- src/main/resources/static/js/app/assignment.js
+++ src/main/resources/static/js/app/assignment.js
@@ -1,10 +1,9 @@
+//java script events to dynamically add new rows  which will consist of questions answers and marks
 $(document).ready(function () {
     var counter = 0;
-
     $("#addrow").on("click", function () {
         var newRow = $("<tr>");
         var cols = "";
-
         cols += '<td><input type="text" class="form-control" name="name' + counter + '"/></td>';
         cols += '<td><input type="text" class="form-control" name="mail' + counter + '"/></td>';
         cols += '<td><input type="text" class="form-control" name="phone' + counter + '"/></td>';
@@ -15,18 +14,32 @@ $(document).ready(function () {
         counter++;
     });
 
-
-
     $("table.order-list").on("click", ".ibtnDel", function (event) {
         $(this).closest("tr").remove();       
         counter -= 1
     });
-
-
 });
+document.getElementById("status").style.display = "none";
+
+
+
+
+
+
+
+    // database call to create new assignment the constructed json will be sent via rest
+    function dbCallToCreateNewAssignment(root){
+           $.ajax({
+            url: "http://localhost:8080/sandbox/login/",
+            data: root,
+            success: function (result) {
+                console.log(result);          
+            },
+            error: function (result) {
+            }
     
-    
-    function saveQuiz(){
-        var childNodes = document.getElementById("myTable").getElementsByTagName("tr");
-        console.log(childNodes);
+          });
+
+           document.getElementById("status").style.display = "block";
+           document.getElementById("status").innerHTML = "Successfully Submitted";
     }
