8d4fa8e675f41a162254fb79cf24608701cd344a
prakharsam <prakharsam97@gmail.com>
2019-11-22 18:18:46 -0700

    US_59 Task#63 Retrieve answer that is dropped by the user

--- src/main/resources/static/js/app/answer.js
+++ src/main/resources/static/js/app/answer.js
@@ -1,3 +1,7 @@
+/*
+* @author:Prakhar Sambhar
+* */
+
 var quizId = localStorage.getItem("quizIndex");
 var quizJson = localStorage.getItem("quizJson");
 localStorage.setItem("quizview", "true");
@@ -15,3 +19,35 @@ original = [];
 result = 0;
 next();
 
+
+//To retrieve answer that has been dropped by the user from the button section
+function getAnswer(){
+    var builtExp ="";
+    var expression = document.getElementById("exp").childNodes;
+    for(i=0; i<expression.length; i++){
+        if(expression[i].nodeName != "#text"){
+            var child = expression[i];
+            var text = child.getElementsByClassName("numberOperatorText")[0];
+
+            if(text != undefined){
+                if(text.innerHTML.replace(" ","") == "x"){
+                    builtExp += "*";
+                    console.log("+ here");
+
+                }
+                else{
+                    builtExp += (text.innerHTML);
+                    console.log(text.innerHTML);
+
+                }
+            }
+
+
+        }
+
+    }
+
+    ans.push(builtExp);
+
+
+}
