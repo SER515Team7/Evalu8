f97a2e6747b4581cefd8e7de6275235e135e95bb
Nabeel Khan <43550603+nakhan8@users.noreply.github.com>
2019-11-27 18:40:17 -0700

    Logout function javascript

---
+++ src/main/resources/static/js/Logout.js
@@ -0,0 +1,27 @@
+
+
+jQuery(document).ready(function() {
+
+    Parse.$ = jQuery;
+    Parse.initialize("...", "...");
+
+    $('.form-logout').on('submit', function (e) {
+        // Prevent Default Submit Event
+        e.preventDefault();
+
+        console.log("Performing submit");
+
+        //logout current user
+        if ( Parse.User.current() ) {
+            Parse.User.logOut();
+
+            // check if really logged out
+            if (Parse.User.current())
+                console.log("Failed to log out!");
+        }
+
+        /
+       
+        window.location.href = "/Sign_In.html";
+    });
+});
