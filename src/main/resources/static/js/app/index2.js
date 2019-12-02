bf32ab7ff12be235636f9da915d16fb1a1530505
Ashutosh Dey <adey13@asu.edu>
2019-12-02 12:05:39 -0700

    US_71 index2.js added drag functionality functions and complex operations

--- src/main/resources/static/js/app/index2.js
+++ src/main/resources/static/js/app/index2.js
@@ -6,11 +6,27 @@ function updateKeyBoard() {
     var numberDoc = document.createDocumentFragment();
     start = dragId;
 
-    $("#numWrapper").append("<div id='deleteElement' class='dropBox deleteDropBox'ondrop='drop(event)' ondragover='allowDrop(event)'></div> <div class='save'> </div><div id='10'  draggable='true' ondragstart='drag(event)' class='operator flexCommon'><span class='numberOperatorText'> +</span></div><div id='11'  draggable='true' ondragstart='drag(event)' class='operator flexCommon'><span class='numberOperatorText'> -</span></div><div id='12'  draggable='true' ondragstart='drag(event)' class='operator flexCommon'><span class='numberOperatorText'> *</span></div><div id='13'  draggable='true' ondragstart='drag(event)' class='operator flexCommon'><span class='numberOperatorText'> /</span></div>  <div id='15'  draggable='true' ondragstart='drag(event)' class='operator flexCommon'><span class='numberOperatorText'> %</span></div>");
+    $("#numWrapper").append("<div id='deleteElement' class='dropBox deleteDropBox'ondrop='drop(event)' ondragover='allowDrop(event)'></div> <div class='save'> </div><div id='10'  draggable='true' ondragstart='drag(event)' class='operator flexCommon'><span class='numberOperatorText'> +</span></div><div id='11'  draggable='true' ondragstart='drag(event)' class='operator flexCommon'><span class='numberOperatorText'> -</span></div><div id='12'  draggable='true' ondragstart='drag(event)' class='operator flexCommon'><span class='numberOperatorText'> *</span></div><div id='13'  draggable='true' ondragstart='drag(event)' class='operator flexCommon'><span class='numberOperatorText'> /</span></div>  <div id='15'  draggable='true' ondragstart='drag(event)' class='operator flexCommon'><span class='numberOperatorText'> %</span></div><div id='19'  draggable='true' ondragstart='drag(event)' class='operator flexCommon' style='width:150px;'><input type ='text' style ='font-size: 25px' class='textBox'/></div>     <div id='20'  draggable='true' ondragstart='drag(event)' class='operator flexCommon' style='width:150px;'><span class='numberOperatorText' style='font-size: 35px'>Sin(</span><input type ='text' style ='font-size: 25px' class='textBox'/><span style='font-size: 35px' class='numberOperatorText'>)</span></div><div id='21'  draggable='true' ondragstart='drag(event)' class='operator flexCommon' style='width:150px;'><span class='numberOperatorText' style='font-size: 35px'>Tan(</span><input type ='text' style ='font-size: 25px' class='textBox'/><span style='font-size: 35px' class='numberOperatorText'>)</span></div><div id='22'  draggable='true' ondragstart='drag(event)' class='operator flexCommon' style='width:150px;'><span class='numberOperatorText' style='font-size: 35px'>Cos(</span><input type ='text'  style ='font-size: 25px' class='textBox'/><span style='font-size: 35px' class='numberOperatorText'>)</span></div>\"");
 
 
 }
 
 var dragId = 14;
 var start = 0;
+
+/*
+    This function allows draggable object to be dropped
+   */
+function allowDrop(ev) {
+    ev.preventDefault();
+
+}
+
+/*
+This function allows object to be dragged
+*/
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+
+}
 
